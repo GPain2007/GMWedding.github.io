6e072c693ab82653fb6b38bcbc964c9cfadd6a4a
Andrew Venson <andrew_venson@scp-health.com>
2022-01-28 22:26:12 -0600

    fixed scrip responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
 function showScrip() {
-  console.log("what's good")
   document.getElementById("scrip-container").style.display = "flex";
   document.getElementById("scrip").style.color = "#d6ca7e"
+  const width = window.innerWidth
+  || document.documentElement.clientWidth
+  || document.body.clientWidth;
+
+  if(width <= 600){
+    document.getElementById("scrip-container").style.display = "none";
+  }
 }
 
 function hideScrip() {
-  console.log("what's good")
   document.getElementById("scrip-container").style.display = "none";
-  document.getElementById("scrip").style.color = "gray"
 }
