596a15cf229333d27f4a03718d7536bf0cb3aee7
Govnor Payne <govnorpayne@gmail.com>
2022-01-30 16:02:23 -0600

    added images

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,41 @@
-function showScrip() {
-  document.getElementById("scrip-container").style.display = "flex";
-  document.getElementById("scrip").style.color = "#d6ca7e"
+function showScrip(language) {
+  
+  
   const width = window.innerWidth
   || document.documentElement.clientWidth
   || document.body.clientWidth;
 
+  if(language ===  "spanish"){
+    document.getElementById("scrip-container-spanish").style.display = "flex";
+    document.getElementById("scrip-spanish").style.color = "#d6ca7e"
+  }else {
+    document.getElementById("scrip-container").style.display = "flex";
+    document.getElementById("scrip").style.color = "#d6ca7e"
+  }
   if(width <= 600){
     document.getElementById("scrip-container").style.display = "none";
+    document.getElementById("scrip-container-spanish").style.display = "none";
   }
 }
 
 function hideScrip() {
   document.getElementById("scrip-container").style.display = "none";
+  document.getElementById("scrip-container-spanish").style.display = "none";
+}
+
+function changeDefaultLanguage() {
+  var name = document.getElementById("button-language").innerHTML;
+  
+  if(name === "Spanish") {
+    document.getElementById("button-language").innerText = "Ingles";
+    document.getElementById("scrip").style.display = "none";
+    document.getElementById("scrip-spanish").style.display = "block";
+  }else{
+    document.getElementById("button-language").innerText = "Spanish";
+    document.getElementById("scrip-spanish").style.display = "none";
+    document.getElementById("scrip").style.display = "block";
+  }
+
+
+
 }
